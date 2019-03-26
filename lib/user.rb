55383c95582ce58b479c406e7eb0b3c6a4628d74
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-03-26 23:58:35 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,10 @@
 class User 
-  attr_accessor :first_name, :last_name
+
+  def first_name=(first_name)
+    @first_name = first_name 
+  end
   
-  def initialize(first_name, last_name)
-    @first_name = first_name
+  def last_name=(last_name)
     @last_name = last_name 
   end
   
